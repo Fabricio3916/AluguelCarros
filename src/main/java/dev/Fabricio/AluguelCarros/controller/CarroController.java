9022bb889951fc0b7ef3fa35ec7cab3976226bf9
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-30 16:54:05 -0300

    feat: implementa as funcionalidades dos Controllers

--- src/main/java/dev/Fabricio/AluguelCarros/controller/CarroController.java
+++ src/main/java/dev/Fabricio/AluguelCarros/controller/CarroController.java
@@ -1,4 +1,68 @@
 package dev.Fabricio.AluguelCarros.controller;
 
+
+import dev.Fabricio.AluguelCarros.model.entity.Carro;
+import dev.Fabricio.AluguelCarros.service.CarroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carros")
 public class CarroController {
+
+    private final CarroService service;
+
+    public CarroController(CarroService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarCarro(@RequestBody Carro novoCarro){
+        Carro carro = service.cadastrar(novoCarro);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Carro cadastrado com sucesso: " + carro.getModelo() + " (ID) : " + carro.getId());
+
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Carro>> listarCarros(){
+        List<Carro> carros = service.listar();
+        return ResponseEntity.ok(carros);
+    }
+
+    @GetMapping("/listarId/{id}")
+    public ResponseEntity<?> listarPorId(@PathVariable Long id){
+        Carro carro = service.listarPorId(id);
+        if (carro != null){
+            return ResponseEntity.ok(carro);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Carro com registro " + id + " não foi encontrado no sistema");
+        }
+    }
+
+    @DeleteMapping("/deletar{id}")
+    public ResponseEntity<String> deletarCarro(@PathVariable Long id){
+        if(service.listarPorId(id) != null){
+            service.deletar(id);
+            return ResponseEntity.ok("Carro deletado com sucesso, seu id era " + id);
+        } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Carro com registro " + id + " não foi encontrado no sistema");
+        }
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarCarro(@PathVariable Long id, @RequestBody Carro carroalterado){
+        if(service.listarPorId(id) != null){
+            Carro novoCarro = service.alterar(id,carroalterado);
+            return ResponseEntity.ok(novoCarro);
+        } else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Carro com registro " + id + " não foi encontrado no sistema");
+        }
+    }
 }
--- src/main/java/dev/Fabricio/AluguelCarros/controller/ClienteController.java
+++ src/main/java/dev/Fabricio/AluguelCarros/controller/ClienteController.java
@@ -1,4 +1,68 @@
 package dev.Fabricio.AluguelCarros.controller;
 
+import dev.Fabricio.AluguelCarros.model.entity.Carro;
+import dev.Fabricio.AluguelCarros.model.entity.Cliente;
+import dev.Fabricio.AluguelCarros.service.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Cliente>> listarTodosClientes(){
+        List<Cliente> clientes = service.listar();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/listarId/{id}")
+    public ResponseEntity<?> listarPorId(@PathVariable Long id){
+        Cliente clienteEncontrado = service.listarPorId(id);
+        if (clienteEncontrado != null){
+            return ResponseEntity.ok(clienteEncontrado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cliente com registro " + id + " não foi encontrado");
+        }
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarRegistro(@RequestBody Cliente cliente){
+        Cliente novoCliente = service.cadastrar(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("O Cliente " + cliente.getNome() + " foi cadastrado com sucesso" );
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarRegistro(@PathVariable  Long id){
+        if (service.listarPorId(id) != null){
+            service.deletar(id);
+            return ResponseEntity.ok("Cliente com registro " + id + " foi deletado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cliente com registro " + id + " não foi encontrado");
+        }
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<String> alterarRegistro(@PathVariable Long id, @RequestBody Cliente clienteAlterado){
+        if (service.listarPorId(id) != null){
+            service.alterar(id,clienteAlterado);
+            return ResponseEntity.ok("Cliente com registro " + id + " foi alterado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cliente com registro " + id + " não foi encontrado");
+        }
+    }
+
 }
