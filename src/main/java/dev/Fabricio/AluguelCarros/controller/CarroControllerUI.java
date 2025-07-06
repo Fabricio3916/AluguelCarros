f5c18eb007b5ca43b03a50581fa39d8d911e1097
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-07-06 15:28:51 -0300

    feat: implementa pagina de listagem de carros e um botão que faz você alternar entre ambas as listas

--- src/main/java/dev/Fabricio/AluguelCarros/controller/CarroControllerUI.java
+++ src/main/java/dev/Fabricio/AluguelCarros/controller/CarroControllerUI.java
@@ -1,4 +1,36 @@
 package dev.Fabricio.AluguelCarros.controller;
 
+import dev.Fabricio.AluguelCarros.model.dto.CarroDTO;
+import dev.Fabricio.AluguelCarros.service.CarroService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("carros/ui")
 public class CarroControllerUI {
+
+    private final CarroService service;
+
+    public CarroControllerUI(CarroService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/listar")
+    public String listarCarros(Model model) {
+        List<CarroDTO> carros = service.listar();
+        model.addAttribute("carros", carros);
+        return "listarCarros";  // Nome da página HTML (listarCarros.html)
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarRegistro(@PathVariable Long id) {
+        service.deletar(id);
+        return "redirect:/carros/ui/listar";
+    }
+
 }
